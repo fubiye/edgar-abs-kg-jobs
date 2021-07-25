16298ccebdcbbac68c04fc6095fb7d91866cd90d
Fu, Biye <biye.fu@outlook.com>
2021-07-25 18:29:35 +0800

    first commit

---
+++ src/main/java/com/biye/edgar/abs/jobs/EdgarAbsKgJobsApplication.java
@@ -0,0 +1,14 @@
+package com.biye.edgar.abs.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgarAbsKgJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgarAbsKgJobsApplication.class, args);
+		System.out.print("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/biye/edgar/abs/jobs/EdgarAbsKgJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biye.edgar.abs.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgarAbsKgJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
